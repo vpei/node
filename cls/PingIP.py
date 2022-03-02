966b89329b393834faf86c4d84a6bd6acff2b172
vpei <vpei@live.com>
2022-03-02 12:05:18 +0800

    Update config for Speed-Node

--- cls/PingIP.py
+++ cls/PingIP.py
@@ -185,7 +185,7 @@ class PingIP():
         try:
             st = time.time()
             # serverStr = '127.0.0.1'
-            port = 8080
+            port = 8088
             proxies = {'http': 'http://localhost:' + str(port),
                         'https': 'http://localhost:' + str(port)}
             session = requests.Session()
