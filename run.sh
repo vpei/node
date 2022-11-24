b6dca651e763ffb59cc94f16cba98fc492489b97
vpei <vpei@live.com>
2022-11-24 23:43:23 +0800

    Update config for Node

--- run.sh
+++ run.sh
@@ -6,4 +6,5 @@ sleep 1
 
 sleep 1
 
-nohup /etc/init.d/nginx start > node.log 2>&1 & 
+# nohup /etc/init.d/nginx start > node.log 2>&1 & 
+nohup python3 /home/node/test.py -u https://ghproxy.com/https://raw.githubusercontent.com/vpei/Free-Node-Merge/main/out/node.txt > node.log 2>&1 & 
